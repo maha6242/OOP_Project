fbc38af6ed1fcd3b5cd319e358b4c07fba70ff26
maha6242 <144605652+maha6242@users.noreply.github.com>
2025-05-07 16:57:15 +0500

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module project2 {
+	requires java.desktop;
+	requires javafx.graphics;
+	requires jakarta.mail;
+	requires jakarta.activation;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	exports com.hamza6dev.oopsieeee;
+}
